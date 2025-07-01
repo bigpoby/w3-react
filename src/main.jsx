9953cd34399e9a7f710e24dafb3aceb2920e1705
shinis(ys) <bigpoby@naver.com>
2025-07-01 11:55:46 +0900

    React Render HTML

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './tutorial/Home.jsx'
+import myelement from './tutorial/RenderHtml.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    {myelement}
   </StrictMode>,
 )
